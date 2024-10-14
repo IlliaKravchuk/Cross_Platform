7d4d6a6f01f1b4e2be59ff902d32726e650943a2
Illia Semenenko <illiak2018@gmail.com>
2024-10-14 14:32:07 +0300

    Add more tests

--- lab1.Tests/UnitTest1.cs
+++ lab1.Tests/UnitTest1.cs
@@ -36,5 +36,46 @@ namespace lab1.Tests
             string result = File.ReadAllText(outputFilePath);
             Assert.Equal("2", result);
         }
+        [Fact]
+        public void Test_IncorrectLength_ShouldThrowException()
+        {
+            // Arrange
+            string inputFilePath = "INPUT.TXT";
+            File.WriteAllText(inputFilePath, "10 2\n100 200 300");
+
+            // Act & Assert
+            Exception ex = Assert.Throws<Exception>(() => CalculateFile.ProcessFiles());
+
+            // Assert message
+            Assert.Equal("Incorrect massive length(massive has less or more values)", ex.Message);
+        }
+
+        [Fact]
+        public void Test_InvalidValue_ShouldThrowException()
+        {
+            // Arrange
+            string inputFilePath = "INPUT.TXT";
+            File.WriteAllText(inputFilePath, "10 3\n100 50000 300"); // 50000 exceeds the allowed range
+
+            // Act & Assert
+            Exception ex = Assert.Throws<Exception>(() => CalculateFile.ProcessFiles());
+
+            // Assert message
+            Assert.Equal("Incorrect value in massive", ex.Message);
+        }
+
+        [Fact]
+        public void Test_LargerThanAllowedInput_ShouldThrowException()
+        {
+            // Arrange
+            string inputFilePath = "INPUT.TXT";
+            File.WriteAllText(inputFilePath, "3201 3\n100 200 300"); // l exceeds the allowed limit
+
+            // Act & Assert
+            Exception ex = Assert.Throws<Exception>(() => CalculateFile.ProcessFiles());
+
+            // Assert message
+            Assert.Equal("Incorrect input L or N", ex.Message);
+        }
     }
 }
--- lab3.Tests/obj/Debug/net8.0/lab3.Tests.AssemblyInfo.cs
+++ lab3.Tests/obj/Debug/net8.0/lab3.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab3.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee4c0270189538a21ee372248bbf2905f86cf7ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab3.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab3.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
